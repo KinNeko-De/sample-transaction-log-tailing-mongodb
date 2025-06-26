5d7c1c2b6dee824e2f476490727be5d1f19b62ae
Nils Diekmann <github@kinneko.de>
2025-06-26 20:51:51 +0200

    - initialize producer

---
+++ producer/go.mod
@@ -0,0 +1,3 @@
+module github.com/KinNeko-De/sample-transaction-log-tailing-mongodb/producer
+
+go 1.24.4
---
+++ producer/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting producer...")
+
+	fmt.Println("Shutting down producer...")
+}
