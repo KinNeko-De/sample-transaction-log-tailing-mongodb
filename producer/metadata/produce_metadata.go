bc25bbb756d2c9254e2cbb5a15dc818928589988
Nils Diekmann <github@kinneko.de>
2025-06-26 21:06:06 +0200

    - add jittered delay

--- producer/metadata/produce_metadata.go
+++ producer/metadata/produce_metadata.go
@@ -3,7 +3,13 @@ package metadata
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"time"
+)
+
+var (
+	minDelay = 2
+	maxDelay = 5
 )
 
 func ProduceFileMetadata(ctx context.Context) error {
@@ -14,8 +20,14 @@ func ProduceFileMetadata(ctx context.Context) error {
 		case <-ctx.Done():
 			fmt.Println("Context cancelled, producing metadata stopped")
 			return ctx.Err()
-		case <-time.After(5 * time.Second):
+		case <-time.After(CreateJitteredDelay()):
 			fmt.Println("File metadata produced")
 		}
 	}
+}
+
+func CreateJitteredDelay() time.Duration {
+	jitter := time.Duration(rand.Intn(maxDelay-minDelay)+minDelay) * time.Second
+	fmt.Printf("Jittered delay: %v\n", jitter)
+	return jitter
 }
