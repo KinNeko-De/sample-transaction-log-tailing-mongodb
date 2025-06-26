3a702cb4c62fc4c81c348f4db3047a55c5f0bc16
Nils Diekmann <github@kinneko.de>
2025-06-26 21:28:18 +0200

    - produce file metadata

--- producer/metadata/produce_metadata.go
+++ producer/metadata/produce_metadata.go
@@ -6,6 +6,9 @@ import (
 	"math/rand"
 	"time"
 
+	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,9 +33,29 @@ func ProduceFileMetadata(ctx context.Context) error {
 			fmt.Println("Context cancelled, producing metadata stopped")
 			return ctx.Err()
 		case <-time.After(CreateJitteredDelay()):
-			fmt.Println("File metadata produced")
+			if err := InsertFileMetadata(ctx); err != nil {
+				return fmt.Errorf("failed to insert file metadata: %v", err)
+			}
 		}
 	}
+}
+
+func InsertFileMetadata(ctx context.Context) error {
+	collection := client.Database("store_file").Collection("file")
+
+	fileId := uuid.New()
+	doc := bson.M{
+		"_id":       primitive.NewObjectID(),
+		"FileId":    primitive.Binary{Subtype: 4, Data: fileId[:]}, // store UUID as BSON binary subtype 4
+		"Extension": ".txt",
+		"MediaType": "text/plain",
+		"Size":      rand.Int63n(10_000_000), // random file size up to 10MB
+		"CreatedAt": time.Now(),
+	}
+
+	_, err := collection.InsertOne(ctx, doc)
+	fmt.Printf("Inserted file metadata: %+v\n", doc)
+	return err
 }
 
 func initializeMongoClient(ctx context.Context) error {
