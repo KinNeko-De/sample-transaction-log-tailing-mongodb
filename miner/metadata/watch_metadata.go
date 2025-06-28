62e15d905d5c4c61a24a47c2e55c8a2ecef698b8
Nils Diekmann <github@kinneko.de>
2025-06-28 09:19:14 +0200

    - miner connect to mongodb

--- miner/metadata/watch_metadata.go
+++ miner/metadata/watch_metadata.go
@@ -3,10 +3,23 @@ package metadata
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	client *mongo.Client
 )
 
 func MiningFileMetadata(ctx context.Context) error {
 	fmt.Println("Mining file metadata...")
+
+	if err := initializeMongoClient(ctx); err != nil {
+		return err
+	}
+	defer disconnectMongoClient()
 
 	for {
 		select {
@@ -15,6 +28,39 @@ func MiningFileMetadata(ctx context.Context) error {
 			return ctx.Err()
 		default:
 			fmt.Println("Watching for file metadata..")
+		}
+	}
+}
+
+func initializeMongoClient(ctx context.Context) error {
+	if client == nil {
+		var err error
+		clientOptions := options.Client().
+			ApplyURI("mongodb://localhost:27017/?replicaSet=rs0")
+		client, err = mongo.Connect(ctx, clientOptions)
+		if err != nil {
+			return fmt.Errorf("failed to connect to MongoDB: %v", err)
+		}
+		fmt.Println("MongoDB client initialized")
+
+		if err := client.Ping(ctx, nil); err != nil {
+			return fmt.Errorf("failed to ping MongoDB: %v", err)
+		}
+		fmt.Println("MongoDB ping successful")
+	}
+	return nil
+}
+
+func disconnectMongoClient() {
+	if client != nil {
+		// Create a new context with timeout for disconnect operation, the application context might be cancelled
+		disconnectCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := client.Disconnect(disconnectCtx); err != nil {
+			fmt.Printf("Failed to disconnect MongoDB client: %v\n", err)
+		} else {
+			fmt.Println("MongoDB client disconnected")
 		}
 	}
 }
