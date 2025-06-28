c8b8d79e50739a5e682ac7cbdbffa7fd2d7cb75f
Nils Diekmann <github@kinneko.de>
2025-06-28 17:05:29 +0200

    - add message broker

--- drop-sut.sh
+++ drop-sut.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-docker compose -f scripts/sut/mongodb.yaml down --remove-orphans --volumes
+docker compose -f scripts/sut/mongodb.yaml -f scripts/sut/kafka.yaml down --remove-orphans --volumes
