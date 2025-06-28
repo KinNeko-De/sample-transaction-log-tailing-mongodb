b103bbb9629db8bd9d7c0516f129a1c337202241
Nils Diekmann <github@kinneko.de>
2025-06-28 17:59:49 +0200

    - initialize consumer

---
+++ consumer/go.mod
@@ -0,0 +1,3 @@
+module github.com/KinNeko-De/sample-transaction-log-tailing-mongodb/consumer
+
+go 1.24.4
---
+++ consumer/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Starting consumer...")
+
+	fmt.Println("Shutting down consumer...")
+}
